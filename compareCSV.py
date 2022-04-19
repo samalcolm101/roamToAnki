634d941a81466f2ec3df8107a2bc836d7d3c5c4f
Sam Malcolm <samalcolm101@gmail.com>
2022-04-19 22:21:58 +0100

    yeah

--- compareCSV.py
+++ compareCSV.py
@@ -1,4 +1,3 @@
-#import pandas as pd
 import csv
 
 nameOfCert = "az900"
@@ -9,19 +8,8 @@ with open(f'{nameOfCert}_full.csv', 'r', encoding='UTF8') as csv1, open(f'{nameO
     import2 = csv.reader(csv2)
     for row in import1:
         if row not in import2:
-            print(row)
             op.append(row)
 
 with open(f'{nameOfCert}_updates.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(op)
-
-
-# print(import1)
-
-
-# with open('data_diff.csv', 'w') as outFile:         # Create CSV file with differences
-# for row in import2:
-#    if row not in import1:
-#        print(row)
-#        outFile.write(row)
