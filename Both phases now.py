9df8dd390def957c5f01988688b13f0fbb763948
Sam Malcolm <samalcolm101@gmail.com>
2022-04-26 16:41:45 +0100

    with comments

--- Both phases now.py
+++ Both phases now.py
@@ -1,46 +1,48 @@
+# Imports
 import json
 import csv
 
+# OP lists
 az900 = []
 dp900 = []
 ai900 = []
 
-
+# Splits a text objcet into its question part and its tag part
 def removeHashTags(text):
     text = text.replace('[', '').replace(']', '').replace(
-        '>', '').replace('"', '')
-    partition = text.partition("#")
+        '>', '').replace('"', '') #removes any punctuation
+    partition = text.partition("#") #splits question from tags
     question = partition[0]
     withouthash = partition[-1].replace('QuizQuestion', "")
-    tags = withouthash.replace('#', '').split()
-    tags = ','.join(tags)
+    tags = withouthash.replace('#', '').split() #removes the #s from the list of tags
+    tags = ','.join(tags) #inserts a comma so it can be joined as a list
     return question, tags
 
-
+# Splits children from a parent object
 def getChildren(parent):
     for y in parent["children"]:
-        if "#QuizQuestion" in y["string"]:
+        if "#QuizQuestion" in y["string"]: # Splits QuizQuestions apart from other children
             question, tags = removeHashTags(y["string"])
-            question = question.split()
-            question = " ".join(question)
-            if "AZ900" in tags:
+            question = question.split() # Removes any whitespace
+            question = " ".join(question) 
+            if "AZ900" in tags: #Appends all AZ900 questions
                 az900.append([question, parent['title'], tags])
-            if "DP900" in tags:
+            if "DP900" in tags: #appends all DP900 questions
                 dp900.append([question, parent['title'], tags])
-            if "AI900" in tags:
+            if "AI900" in tags: #appends all AI900 questions
                 ai900.append([question, parent['title'], tags])
 
-
+#splits parents (including children) from the JSON OBJ
 def getParents(jsonObj):
     for items in jsonObj:
         try:
-            if len(items["children"]) > 0:
+            if len(items["children"]) > 0: #if the parent has children
                 getChildren(items)
         except:
-            continue
+            continue #Ignore if it doesnt have children
     createCSV()
 
-
+#creates the CSVs from the OP lists
 def createCSV():
     with open('az900.csv', 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
--- compareCSV.py
+++ compareCSV.py
@@ -1,15 +1,17 @@
 import csv
 
+#name of the cert you are comparing
 nameOfCert = "az900"
 op = []
 
+#Opens the full library CSV
 with open(f'{nameOfCert}_full.csv', 'r', encoding='UTF8') as csv1, open(f'{nameOfCert}.csv', 'r', encoding='UTF8') as csv2:
     import1 = csv.reader(csv1)
     import2 = csv.reader(csv2)
-    for row in import1:
+    for row in import1: #if question from new CSV isn't in the full library then append to OP list
         if row not in import2:
             op.append(row)
 
-with open(f'{nameOfCert}_updates.csv', 'w', encoding='UTF8', newline='') as f:
+with open(f'{nameOfCert}_updates.csv', 'w', encoding='UTF8', newline='') as f: #Creates a list of just the differences
     writer = csv.writer(f)
     writer.writerows(op)
